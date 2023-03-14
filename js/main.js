8a6060bccafe75033d9d5a87e6fb593a5d93ef54
mspolermo <mspolermo@gmail.com>
2023-03-14 16:29:34 +0500

    Layout v.021

--- js/main.js
+++ js/main.js
@@ -3,12 +3,12 @@ let commentBlock = document.getElementById('comment-block');
 let textarea = document.getElementById('text-of-comment');
 let form = document.forms.formCreate;
 
-btnSend.addEventListener('click', function () {
+btnSend.addEventListener('click', function (event) {
     event.preventDefault();
-    validationCheck('name', 2, true);
+    validationCheck('name' , 2 , true);
     validationCheck('text-of-comment');
 
-    if (validationCheck('name', 2, true) && validationCheck('text-of-comment'))  {
+    if (validationCheck('name' , 2 , true) && validationCheck('text-of-comment'))  {
         createComment ();
     };
 });
@@ -16,17 +16,18 @@ btnSend.addEventListener('click', function () {
 commentBlock.addEventListener('click', function(event) {
 
     if (event.target.className == 'section-comments-list__like') {
+
         if (!event.target.getAttribute('src').includes('-active')) {
 
             const passiveLike = event.target.closest('div');
-
             const activeLike = document.createElement('div');
             activeLike.className = 'section-comments-list__icon';
             activeLike.innerHTML = '<img src="./img/comments-like-active.svg" alt="" class="section-comments-list__like">';
             passiveLike.replaceWith(activeLike);
-        } else {
-            const activeLike = event.target.closest('div');
 
+        } else {
+
+            const activeLike = event.target.closest('div');
             const passiveLike = document.createElement('div');
             passiveLike.className = 'section-comments-list__icon';
             passiveLike.innerHTML = '<img src="./img/comments-like.svg" alt="" class="section-comments-list__like">';
@@ -34,32 +35,37 @@ commentBlock.addEventListener('click', function(event) {
         }
     };
 
-    if (event.target.className != 'section-comments-list__remove') return;
+    if (event.target.className == 'section-comments-list__remove') {
 
-    let currentComment = event.target.closest('.section-comments-list__comment');
-    currentComment.remove();
-    document.getElementById('section-hero-comments').innerText = commentsCount();
-    document.getElementById('section-comment-create-counter').innerText = commentsCount();
+        let currentComment = event.target.closest('.section-comments-list__comment');
+        currentComment.remove();
+        document.getElementById('section-hero-comments').innerText = commentsCount();
+        document.getElementById('section-comment-create-counter').innerText = commentsCount();
+
+    };
 });
 
-textarea.addEventListener('keydown', function () {
+textarea.addEventListener('keydown', function (event) {
     if (this.scrollHeight > 20) {
         this.style.cssText = 'height: auto; padding: 0';
         this.style.cssText = 'height:' + (this.scrollHeight + 6)+ 'px';
     };
+
     if (event.keyCode == 13) {
         event.preventDefault();
-        validationCheck('name', 2, true);
+        validationCheck('name' , 2 , true);
         validationCheck('text-of-comment');
     
-        if (validationCheck('name', 2, true) && validationCheck('text-of-comment'))  {
+        if (validationCheck('name' , 2 , true) && validationCheck('text-of-comment'))  {
             createComment ();
         };
     };
 });
 
-function createComment (event) {
-    let numberOfComment = commentsCount () + 1;
+let numberOfComment = commentsCount ();
+
+function createComment () {
+    numberOfComment += 1;
 
     const newComment = document.createElement('div');
     newComment.className = 'section-comments-list__comment';
@@ -118,7 +124,7 @@ function commentsCount () {
    return commentBlock.children.length
 };
 
-function validationCheck (id, min = 1, symbols = false) {
+function validationCheck (id , min = 1 , symbols = false) {
     let field = document.getElementById(id);
 
     if ((field.value.length < min) || (symbols == true)) {
@@ -131,72 +137,79 @@ function validationCheck (id, min = 1, symbols = false) {
             field.addEventListener('input', function () {
                 document.getElementById('label-for-' + id).classList.add('section-comment-create__error_none');
             });
-            return false
+            return false;
         };
 
         if (symbols == true){
 
             if (field.value.match(/[0-9\\.,:*$/\\!@#%^&()'"]/g) !== null) {
+
                 document.getElementById('label-for-' + id).classList.remove('section-comment-create__error_none');
                 document.getElementById('label-for-' + id).innerText = `Поле не должно содержать цифр и спецсимволов`;
 
                 field.addEventListener('input', function () {
                     document.getElementById('label-for-' + id).classList.add('section-comment-create__error_none');
                 });
-                return false
+                return false;
             }else {
-                return true
+                return true;
             };
         }; 
-
     } else {
-        return true
+        return true;
     };
 };
 
 function dateUpdate (date) {
     let today = new Date();
-    let yesterday = daysAgoCount(today, 1);
+    let yesterday = daysAgoCount(today , 1);
 
-    function daysAgoCount(today, num) {
+    function daysAgoCount(today , num) {
         let month = today.getMonth() + 1;
-        if (month <10) {
-            month='0'+ (today.getMonth() + 1);
+        if (month < 10) {
+            month='0' + (today.getMonth() + 1);
         };
         let day = today.getDate() - num;
-        if (day<10) {
+        if (day < 10) {
             day= '0' + (today.getDate() - num);
         }
-        return `${today.getFullYear()}-${month}-${day}`
+        return `${today.getFullYear()}-${month}-${day}`;
     };
+
     function randomTime() {
-        let randomHours = Math.floor(Math.random()*(24-0) + 0);
-        let randomMinutes = Math.floor(Math.random()*(60-0) + 0);
+
+        let randomHours = Math.floor(Math.random() * (24 - 0) + 0);
+        let randomMinutes = Math.floor(Math.random() * (60 - 0) + 0);
+
         if (randomHours < 10) {
             randomHours = '0' + randomHours;
         };
+
         if (randomMinutes < 10) {
             randomMinutes = '0' + randomMinutes;
         };
-        return `${randomHours}:${randomMinutes}`
-    }
-
+        return `${randomHours}:${randomMinutes}`;
+    };
 
     if ((date == '') || (date == daysAgoCount(today, 0))) {
         let hours = today.getHours();
         let minutes = today.getMinutes();
+
         if (hours < 10) {
             hours = '0' + hours;
         };
+
         if (minutes < 10) {
             minutes = '0' + minutes;
         };
+
         date = `сегодня, ${hours}:${minutes}`;
+
     }else if (date == yesterday) {
         date = 'вчера, ' + randomTime();
     } else {
         date = date + ', ' + randomTime();
-    }
+    };
 
-    return date
+    return date;
 };
